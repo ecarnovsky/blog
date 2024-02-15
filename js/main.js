4ecbbf7cea148bc4d87af8d88c4241a962617ed6
ecarnovsky <ecarnovsky@gmail.com>
2024-02-15 01:12:31 -0500

    Added some shadows to text and the main image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+let socialMediaIcons = document.querySelectorAll('footer a')
 
+for(let i = 0; i < socialMediaIcons.length; i++){
+    socialMediaIcons[i].addEventListener('mouseover', togglePopOut)
+    socialMediaIcons[i].addEventListener('mouseout', togglePopOut)
+}
+
+function togglePopOut(){
+    this.firstChild.classList.toggle("fa-beat")
+}
